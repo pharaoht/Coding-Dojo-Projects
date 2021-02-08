c8eacb4be35621e8e525f1ee14c394da9692a95c
Pharaoh <pharaohmanson@gmail.com>
2021-02-08 13:35:55 -0500

    Added virtual environments and learning basics of python

---
+++ Python_Stack/_python/python_fundamentals/printing_strings/python.py
@@ -0,0 +1,3 @@
+#Strings are any sequence of characters (letters, numerals, ~($/}\#, etc.) enclosed in single or double quotes. We can display a string like this:
+print("this is a sample string")
+
