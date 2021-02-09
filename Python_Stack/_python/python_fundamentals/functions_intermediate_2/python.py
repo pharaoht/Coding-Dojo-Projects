fa03dbf333de688f48291f3a8c7a95148d290276
Pharaoh <pharaohmanson@gmail.com>
2021-02-09 18:23:00 -0500

    python practice

--- Python_Stack/_python/python_fundamentals/functions_intermediate_2/python.py
+++ Python_Stack/_python/python_fundamentals/functions_intermediate_2/python.py
@@ -64,3 +64,11 @@ dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
+
+
+def printInfo(some_dict):
+    for key in dojo:
+        print(key, dojo[key])
+
+
+printInfo(dojo)
