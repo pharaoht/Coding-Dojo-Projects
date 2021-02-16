735bfed1cdd39321c966fde114494afef49d08f8
Pharaoh <pharaohmanson@gmail.com>
2021-02-16 17:42:53 -0500

    Django files

--- Python_Stack/flask/flask_demo/hello.py
+++ Python_Stack/flask/flask_demo/hello.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 @app.route('/hello/<name>/<num>')
 def hello(num, name):
-    return render_template("index.html", num=int(num), name=name)
+    return render_template("index3.html", num=int(num), name=name)
 
 
 @app.route('/user/<username>/<id>')
@@ -22,6 +22,15 @@ def show_user_profile(username, id):
 @app.errorhandler(404)
 def page_not_found(e):
     return
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    print(request.form)
+    name_from_form = request.form['name']
+    email_from_form = request.form['email']
+    return render_template("show.html", name_on_template=name_from_form, email_on_template=email_from_form)
 
 
 if __name__ == "__main__":
