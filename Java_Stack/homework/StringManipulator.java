84dd98a9ca7b7935998458de76be895626889e3c
pharaoh <pharaohmanson@gmail.com>
2021-04-06 19:49:53 -0400

    java assignment

---
+++ Java_Stack/homework/StringManipulator.java
@@ -0,0 +1,19 @@
+package Java_Stack.homework;
+
+public class StringManipulator {
+
+    public static String trimAndConcat(String String1, String String2) {
+        String str = trimAndConcat(String1, String2);
+        return str;
+    }
+
+    public static Integer getIndexOrNull(String String1, char Char1) {
+        Integer index = getIndexOrNull(String1, Char1);
+        return index;
+    }
+
+    public static Integer getIndexOrNull(String String1, String String2) {
+        Integer index = getIndexOrNull(String1, String2);
+        return index;
+    }
+}
