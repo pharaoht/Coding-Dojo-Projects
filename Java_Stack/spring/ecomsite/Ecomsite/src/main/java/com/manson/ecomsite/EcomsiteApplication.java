b16fe7353389211806257cb9d706618d27cbea90
pharaoh <pharaohmanson@gmail.com>
2021-05-21 12:13:07 -0400

    test

---
+++ Java_Stack/spring/ecomsite/Ecomsite/src/main/java/com/manson/ecomsite/EcomsiteApplication.java
@@ -0,0 +1,13 @@
+package com.manson.ecomsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomsiteApplication.class, args);
+	}
+
+}
---
+++ Java_Stack/spring/ecomsite/Ecomsite/src/test/java/com/manson/ecomsite/EcomsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manson.ecomsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
