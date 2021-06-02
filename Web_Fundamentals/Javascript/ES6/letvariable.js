b2dfdddd1647b446adca87862ca84c2baa0f53bc
pharaoh <pharaohmanson@gmail.com>
2021-06-02 14:43:07 -0400

    let variable topic ed6

---
+++ Web_Fundamentals/Javascript/ES6/letvariable.js
@@ -0,0 +1,10 @@
+var topic = "JavaScript"
+
+if(topic){
+    let topic = "ECMAScript"
+    console.log("Block scope", topic)
+}
+
+console.log("global scope", topic)
+
+//let keyword adds a block scope to the variable, and the value is only changed inside the block/function 
