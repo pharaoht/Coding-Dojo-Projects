504f71acbac3c006cf866a08aea0566058f81ce2
pharaoh <pharaohmanson@gmail.com>
2021-06-07 20:54:09 -0400

    es6 practice

--- Web_Fundamentals/Javascript/ES6/letvariable.js
+++ Web_Fundamentals/Javascript/ES6/letvariable.js
@@ -8,3 +8,4 @@ if(topic){
 console.log("global scope", topic)
 
 //let keyword adds a block scope to the variable, and the value is only changed inside the block/function 
+//also helps enforce block scope in your code
